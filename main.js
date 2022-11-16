20413856e1dde0cff4e99b1b3db80aebbafdfefc
26Kevin <k.patel46@share.epsb.ca>
2022-11-16 11:13:56 -0700

    updating

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+let charOutputEl = document.getElementById("output");
 
+//Input
+document.getElementById("btn").addEventListener("click", btnClicked);
+function btnClicked() {
+  //Quesetion 1
+  let question1 = document.getElementById("q1").ariaValueMax.toLowerCase();
+}
