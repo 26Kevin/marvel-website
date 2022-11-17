fb451b28180027f73feb3ab54b064a75049d9cd5
26Kevin <k.patel46@share.epsb.ca>
2022-11-17 11:23:34 -0700

    updating

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
+let numCounterEl = document.getElementById("num-counter");
 let charOutputEl = document.getElementById("output");
 
+let numCounter = 0;
 //Input
 document.getElementById("btn").addEventListener("click", btnClicked);
 function btnClicked() {
   //Quesetion 1
-  let question1 = document.getElementById("q1").ariaValueMax.toLowerCase();
+  let answer1 = document.getElementById("q1").value.toLowerCase();
+
+  //If Statement
+  if (answer1 === "peter parker") {
+    numCounter++
+  } else {
+    let numCounterEl.innerHTML = "Your score:0/4 (0%)" 
+  }
+
+  //Question 2
+  let answer2 = document.getElementById("q2").value.toLowerCase();
+
+  //If Statement
+  if (answer2 === "iron man") {
+    charOutputEl.innerHTML = numCounter++;
+  } else {
+    charOutputEl.innerHTML = "Your score:0/4 (0%)";
+  }
+
+  //Question 3
+  let answer3 = document.getElementById("q3").value.toLowerCase();
+
+  // If Statement
+  if (answer3 === "vibranium") {
+    charOutputEl.innerHTML = numCounter++;
+  } else {
+    charOutputEl.innerHTML = "Your socre:0/4 (0%)";
+  }
+
+  //Question 4
+  let answer4 = document.getElementById("q4").value.toLowerCase();
+
+  //If Statement
+  if (answer4 === "tony stark") {
+    charOutputEl.innerHTML = numCounter++;
+  } else {
+    charOutputEl.innerHTML = "Your socre:0/4 (0%)";
+  }
 }
